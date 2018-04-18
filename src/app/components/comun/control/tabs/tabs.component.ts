ef05ae0685b567d5719ecf5c3cc31c54002a7b3b
Bernardo <bernamp14@hotmail.com>
2018-04-18 12:46:23 -0500

    componentes de tabs

--- src/app/components/comun/control/tabs/tabs.component.ts
+++ src/app/components/comun/control/tabs/tabs.component.ts
@@ -5,13 +5,23 @@ import { Component, Input, OnInit } from '@angular/core';
   templateUrl: './tabs.component.html',
   styleUrls: ['./tabs.component.css']
 })
-export class TabsComponent implements OnInit {
-  @Input() Opciones:String[];
-  constructor() { }
 
-  //Opciones:String[] = ["Mis Archivos", "Inquietudes", "Apuntes", "Destacados"]
+export class TabsComponent implements OnInit {
+  @Input() Opciones:String[]
+  
+  constructor() {}
+  
+  contenidos:string[] = [];  
+  // Opciones:String[] = ["Mis Archivos", "Inquietudes", "Apuntes", "Destacados"]
 
   ngOnInit() {
   }
+  
+  funcion(tab:string){
+    
+    this.contenidos = [];    
+    this.contenidos.push(`Data del servicio. param ` + tab );
+    
+  } 
 
 }
